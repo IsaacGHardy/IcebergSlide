ee3919a5d2452888ef48ac593a45401f5457c2c4
JAldrich650 <98219797+JAldrich650@users.noreply.github.com>
2024-01-23 21:02:54 -0600

    AI Skeleton
    Base layout and decisions for the AI

---
+++ Capstone - Team 1/Assets/AI.py
@@ -0,0 +1,20 @@
+#NOTES:
+#AI Should always go first as the 3x3 and 4x4 versions of Quixo
+#are first player win games, and so are the majority of board games
+
+#START:
+#Pickle to save
+#numpy
+#tensorflow from tensorflow.keras import layers
+
+#Deep Reinforcement Learning or Value Iteration (VI)
+
+
+#Evaluate a given 5x5 grid
+    #Know what piece you are (X or O)
+    #Reward more for controlling middle +(25, 15, 5)
+    #Control Corners +(20)
+
+#GetBestMove
+    #Should take a board, team to move for, and difficulty
+
