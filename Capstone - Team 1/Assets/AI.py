c248e63e9a478e294c711a9a9917535da7f1c195
JAldrich650 <98219797+JAldrich650@users.noreply.github.com>
2024-02-01 22:25:21 -0600

    Basics for Python Quixo Gameplay
    Creating a basic game of Quixo to test the AI on

--- Capstone - Team 1/Assets/AI.py
+++ Capstone - Team 1/Assets/AI.py
@@ -18,4 +18,40 @@
 #GetBestMove
     #Should take a board, team to move for, and difficulty
 
-#Useless change to see if it goes through
+#https://neptune.ai/blog/saving-trained-model-in-python#:~:text=To%20save%20a%20deep%20learning,loaded%20later%20to%20make%20predictions.
+#https://www.activestate.com/resources/quick-reads/how-to-create-a-neural-network-in-python-with-and-without-keras/
+
+#Quxio but in Python
+import numpy
+
+board = [[0]*5]*5
+
+def request_input(prompt, acceptable_responses):
+    result = None
+    while (result not in acceptable_responses):
+        result = input(prompt)
+    return result
+
+def print_board():
+    for row in board:
+        print(row)
+    
+def check_for_win():
+    return {} #Tuple of winners
+
+def start_match():
+    current_move = "X"
+    has_winner = False
+    
+    player_count = int(request_input("Would you like 2 or 4 players for this match ", {'2', '4'}))
+    x_or_o = request_input("Type your team: X or O ", {'x', 'o', 'X', 'O'})
+
+    while (has_winner):
+        #play move 
+        #check fo a win
+        if ("X" or "O" in check_for_win):
+            #Check for the winner and output it
+            has_winner = True
+
+
+start_match()
