4b78607de7231c65106dcfbd7484ac7bc0571ba4
JAldrich650 <98219797+JAldrich650@users.noreply.github.com>
2024-02-03 15:22:22 -0600

    Tensorflow library issue
    Debugging my tensorflow version mismatch

--- Capstone - Team 1/Assets/AIComp/AIBot.py
+++ Capstone - Team 1/Assets/AIComp/AIBot.py
@@ -2,6 +2,5 @@
 #https://neptune.ai/blog/saving-trained-model-in-python#:~:text=To%20save%20a%20deep%20learning,loaded%20later%20to%20make%20predictions.
 #https://www.activestate.com/resources/quick-reads/how-to-create-a-neural-network-in-python-with-and-without-keras/
 
-from tensorflow import keras
-from keras.models import Sequential, model_from_json
-from keras.layers import Dense
+from tensorflow.keras.models import Sequential, model_from_json
+from tensorflow.keras.layers import Dense
