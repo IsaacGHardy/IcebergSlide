db5c4e049884885618173f76a18e358d266bb455
JAldrich650 <98219797+JAldrich650@users.noreply.github.com>
2024-02-03 15:11:03 -0600

    (ignore) Config
    ignore

--- Capstone - Team 1/Assets/AIComp/AIBot.py
+++ Capstone - Team 1/Assets/AIComp/AIBot.py
@@ -2,13 +2,6 @@
 #https://neptune.ai/blog/saving-trained-model-in-python#:~:text=To%20save%20a%20deep%20learning,loaded%20later%20to%20make%20predictions.
 #https://www.activestate.com/resources/quick-reads/how-to-create-a-neural-network-in-python-with-and-without-keras/
 
-print("Yay")
-
-#from keras.models import Sequential
-#from keras.layers import Dense, Activation
-
-import tensorflow as tf
 from tensorflow import keras
-
-print(tf.__version__)
-print(keras.__version__)
+from keras.models import Sequential, model_from_json
+from keras.layers import Dense
