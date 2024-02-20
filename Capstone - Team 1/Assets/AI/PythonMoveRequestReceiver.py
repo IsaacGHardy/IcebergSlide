d164b8989db011ede53e77d0f3865eaad5b58662
JAldrich <JAldrich650@gmail.com>
2024-02-20 17:51:46 -0600

    READY FOR ANOTHER EMAIL

--- Capstone - Team 1/Assets/AI/PythonMoveRequestReceiver.py
+++ Capstone - Team 1/Assets/AI/PythonMoveRequestReceiver.py
@@ -1,5 +1,3 @@
-print("Python Script Running")
-
 from AI import request_ai_move
 
 def chars_to_board(chars):
@@ -13,12 +11,11 @@ def chars_to_board(chars):
 
 #CSharp_input will contain 26 chars, The 1st 25 will be board data, the final char is the team to generate a move for
 CSharp_input = input()
-#CSharp_input = "                    O   XX"
+#CSharp_input = "   XO                    X"
 
 team_playing_for = CSharp_input[25:26:1]
 board_data = CSharp_input[0:25:1]
 generated_board = chars_to_board(board_data)
-generated_board = [list(x) for x in list(zip(*generated_board))]
+#generated_board = [list(x) for x in list(zip(*generated_board))]
 
-#print(generated_board)
 print(request_ai_move(generated_board, team_playing_for))
