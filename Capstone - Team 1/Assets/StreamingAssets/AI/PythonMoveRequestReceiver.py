bca5c0f683416ee0d1c071d19ffec5ec63245cb6
JAldrich650 <98219797+JAldrich650@users.noreply.github.com>
2024-04-02 20:41:17 -0500

    Editing Info Comments

--- Capstone - Team 1/Assets/StreamingAssets/AI/PythonMoveRequestReceiver.py
+++ Capstone - Team 1/Assets/StreamingAssets/AI/PythonMoveRequestReceiver.py
@@ -1,12 +1,12 @@
 from AI import request_ai_move, chars_to_board
 
-#28 Chars Incoming
+#27 Chars Incoming
 #The first 25 chars are the board
 #The 26th is what player the move is for
 #The 27th is the difficulty (0 Hardest - 5 Silly)
 
-CSharp_input = "X                        O0" #Test Data
-#CSharp_input = input()
+#CSharp_input = "X                        O0" #Test Data
+CSharp_input = input()
 
 #Breaking down the incoming data
 board_data = CSharp_input[0:25:1]
