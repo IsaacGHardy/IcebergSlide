87a163e92cec065688b6586ec00564c55a505693
Chloe Elyse <cdalrymple@harding.edu>
2024-04-02 22:02:43 -0500

    Main Menu Help Updates (will change in game help tomorrow morning)

--- Capstone - Team 1/Assets/StreamingAssets/AI/PythonMoveRequestReceiver.py
+++ Capstone - Team 1/Assets/StreamingAssets/AI/PythonMoveRequestReceiver.py
@@ -1,27 +1,21 @@
-from AI import request_ai_move, chars_to_board
+from AI import request_ai_move
 
-#28 Chars Incoming
-#The first 25 chars are the board
-#The 26th is what player the move is for
-#The 27th is the difficulty (0 Hardest - 5 Silly)
-#The 28th tells if we are in the tutorial (0 is not, 1 is hard, 2 is a silly tutorial move)
+def chars_to_board(chars):
+    grid = [['' for _ in range(5)] for _ in range(5)]
 
-#CSharp_input = "X                        O00" #Test Data
+    for i in range(5):
+        for j in range(5):
+            grid[i][j] = chars[i * 5 + j]
+
+    return grid
+
+#CSharp_input will contain 26 chars, The 1st 25 will be board data, the final char is the team to generate a move for
 CSharp_input = input()
+#CSharp_input = "X                        O0"
 
-#Breaking down the incoming data
+difficulty = int(CSharp_input[26:27:1])
+team_playing_for = CSharp_input[25:26:1]
 board_data = CSharp_input[0:25:1]
 generated_board = chars_to_board(board_data)
-team_playing_for = CSharp_input[25:26:1]
-difficulty = int(CSharp_input[26:27:1])
-tutorial = int(CSharp_input[27:28:1])
 
-if (tutorial == 0):
-    #Normal Gameplay Move
-    print(request_ai_move(generated_board, team_playing_for, difficulty))
-elif (tutorial == 1):
-    #Hard Move For Tutorial
-    print(request_ai_move(generated_board, team_playing_for, 0))
-elif (tutorial == 2):
-    #Easy Move For Tutorial
-    print(request_ai_move(generated_board, team_playing_for, 7))
+print(request_ai_move(generated_board, team_playing_for, difficulty))
