1bb26a0f5fbb0ba423738bb781aa33273d170d02
JAldrich650 <98219797+JAldrich650@users.noreply.github.com>
2024-04-02 20:26:42 -0500

    AI configurations for Char length
    a

--- Capstone - Team 1/Assets/StreamingAssets/AI/PythonMoveRequestReceiver.py
+++ Capstone - Team 1/Assets/StreamingAssets/AI/PythonMoveRequestReceiver.py
@@ -4,24 +4,14 @@ from AI import request_ai_move, chars_to_board
 #The first 25 chars are the board
 #The 26th is what player the move is for
 #The 27th is the difficulty (0 Hardest - 5 Silly)
-#The 28th tells if we are in the tutorial (0 is not, 1 is hard, 2 is a silly tutorial move)
 
-#CSharp_input = "X                        O00" #Test Data
-CSharp_input = input()
+CSharp_input = "X                        O0" #Test Data
+#CSharp_input = input()
 
 #Breaking down the incoming data
 board_data = CSharp_input[0:25:1]
 generated_board = chars_to_board(board_data)
 team_playing_for = CSharp_input[25:26:1]
 difficulty = int(CSharp_input[26:27:1])
-tutorial = int(CSharp_input[27:28:1])
 
-if (tutorial == 0):
-    #Normal Gameplay Move
-    print(request_ai_move(generated_board, team_playing_for, difficulty))
-elif (tutorial == 1):
-    #Hard Move For Tutorial
-    print(request_ai_move(generated_board, team_playing_for, 0))
-elif (tutorial == 2):
-    #Easy Move For Tutorial
-    print(request_ai_move(generated_board, team_playing_for, 7))
+print(request_ai_move(generated_board, team_playing_for, difficulty))
